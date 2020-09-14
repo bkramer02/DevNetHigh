16d115448ebbc0981e7206686480ac8522fd5471
Brian <4766518+bkramer02@users.noreply.github.com>
2020-09-14 10:51:25 -0600

    Update challenge.py
    Third Commit

--- wk2/wk2Challenge/challenge.py
+++ wk2/wk2Challenge/challenge.py
@@ -9,20 +9,3 @@ jsonResponse = r.json()
 print(f'The random Chuck Norris joke is: {jsonResponse["value"]}')
 
 
-
-#print(r.status_code)
-#print(r.values)
-
-""" 
-jsonResponse = r.json()
-print("Entire JSON response")
-print(jsonResponse)
-
-print("Print each key-value pair from JSON response")
-for key, value in jsonResponse.items():
-    print(key, ":", value)
-
-print("Access directly using a JSON key name")
-print("Joke is ")
-print(jsonResponse["value"])
- """
